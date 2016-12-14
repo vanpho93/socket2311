6dc9c693d1cd0482b8c58ed2da99d271a2edce7c
Admin <vanpho02@gmail.com>
2016-12-14 09:49:21 +0700

    xong

--- index.js
+++ index.js
@@ -6,14 +6,25 @@ app.set('views', './views');
 app.use(express.static('public'));
 
 var io = require('socket.io')(server); //1
-
+var mang = [];
 server.listen(3000, () => console.log('Server started'));
 
 app.get('/', (req, res) => res.render('home'));
 
 io.on('connection', socket => { //3
   console.log('Co nguoi ket noi');
-  socket.on('CLIENT_SEND_MESSAGE', data => {
-    console.log(data);
-  })
+  socket.on('NGUOI_DUNG_DANG_KY', username => {
+    if(mang.indexOf(username) == -1){
+      mang.push(username);
+      socket.emit('SERVER_XAC_NHAN_DANG_KY', true);
+      io.emit('NGUOI_DUNG_MOI', username);
+    }else{
+      socket.emit('SERVER_XAC_NHAN_DANG_KY', false);
+    }
+    console.log(username);
+  });
 })
+
+/*
+  Username khong duoc trung nhau
+*/
